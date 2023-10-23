012c9d2013ebc3deb4dcacc07fca72ab0d6eaa8e
beahuszar <beata.huszar@recart.com>
2023-10-23 17:10:06 +0200

    implement Viki schedule

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,59 @@
 import './App.css'
-import logo from './logo.svg'
 
 function App() {
+  /* 
+      paratlan: h sz cs 
+      paros: h k cs p sz v
+  
+  */
+
+  const now = new Date()
+  const weekNumber = Math.ceil(
+    (now.getTime() - new Date(now.getFullYear(), 0, 1).getTime()) / 604800000
+  )
+  const isEvenWeek = weekNumber % 2 === 0
+  const daysOfWeek = ['Vasárnap', 'Hétfő', 'Kedd', 'Szerda', 'Csütörtök', 'Péntek', 'Szombat']
+  const dayOfWeek = daysOfWeek[now.getDay()]
+
+  const vikiSchedule = {
+    even: [1, 2, 4, 5, 6, 0],
+    odd: [1, 3, 4]
+  }
+
+  const schedule = [
+    '18:00 - 20:00',
+    '10:00 - 20:00',
+    '10:00 - 20:00',
+    '10:00 - 20:00',
+    '10:00 - 20:00',
+    '10:00 - 20:00',
+    '10:00 - 12:00'
+  ]
+
+  const isVikiWorking = isEvenWeek
+    ? vikiSchedule.even.includes(now.getDay())
+    : vikiSchedule.odd.includes(now.getDay())
+  const thisWeekSchedule = isEvenWeek ? vikiSchedule.even : vikiSchedule.odd
+
   return (
     <div className='App'>
       <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>Ma dolgozik Viki?</p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
+        <h1>Ma dolgozik Viki?</h1>
       </header>
+      <div className='card'>
+        <div>{isVikiWorking ? 'Igen' : 'Nem'}</div>
+        <div className='time'>{isVikiWorking ? schedule[now.getDay()] : '-'}</div>
+        <div className='emoji'>{isVikiWorking ? '👩🏻‍⚕️' : '🎉'}</div>
+      </div>
+      <div>Milyen napokon dolgozik a héten?</div>
+      <p>{thisWeekSchedule.map((day) => daysOfWeek[day]).join(', ')}</p>
+      <footer>
+        <p>
+          A változtatás jogát fenntartjuk, szabadság és ünnepnapok jelenleg nincsenek kezelve a
+          rendszerben. Rugalmasságukat és megértésüket köszönjük.
+        </p>
+        <p>© {now.getFullYear()}</p>
+      </footer>
     </div>
   )
 }
